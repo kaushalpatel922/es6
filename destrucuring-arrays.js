63c6c43139422d68e147131150d66f79470757a4
Kaushal Patel <kaushalpatel922@gmail.com>
2017-06-27 12:17:33 -0400

    destructuring array-adding to variable

---
+++ destrucuring-arrays.js
@@ -0,0 +1,14 @@
+//Destructuring Arrays
+
+// Initialize the variables `one`, `two` and `three
+// with colors from the `things`array
+
+const things = ['red', 'basketball', 'green', 'computer', 'earth', 'blue'];
+
+//setting variables and allowing to skip using comma and setting equal to things.
+const [one,, two,,, three] = things;
+
+//using template literals and assigning to const
+const colors = `List of Colors: ${one}, ${two}, ${three}`;
+
+console.log(colors);
