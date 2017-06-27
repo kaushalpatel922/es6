cc05d8d811d83778d43193783bec517ff87eae6f
Kaushal Patel <kaushalpatel922@gmail.com>
2017-06-27 13:25:15 -0400

    update

--- forofloop.js
+++ forofloop.js
@@ -9,4 +9,15 @@ for (const digit of digits) {
   console.log(digit);
 }
 
-//print 1, 3, 5
+//prints 1, 3, 5
+
+
+//Print days with first letter being capitalized
+const days = ['sunday','monday','tuesday'];
+
+for (let day of days) {
+  day = day.charAt(0).toUpperCase() + day.slice(1);
+  console.log(day);
+}
+
+//prints Sunday Monday Tuesday
